d22e411963a2d40624e083bb4eebc9676361f31f
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2022-04-17 19:29:10 +0900

    Update proc.c

--- proc.c
+++ proc.c
@@ -540,14 +540,14 @@ cps()
   sti();
 
   acquire(&ptable.lock);
-  cprintf("name \t pid \t state \t \n");
+  cprintf("name \t pid \t state \t\t memory \n");
   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
 	if( p->state== SLEEPING)
-         cprintf("%s \t %d  \t SLEEPING \t \n",p->name,p->pid);
+         cprintf("%s \t %d  \t SLEEPING \t %d \n",p->name,p->pid,p->sz);
 	else if(p->state == RUNNING)
-	 cprintf("%s \t %d  \t RUNNING  \t \n",p->name,p->pid);
+	 cprintf("%s \t %d  \t RUNNING  \t %d \n",p->name,p->pid,p->sz);
 	else if(p->state == RUNNABLE)
-	 cprintf("%s \t %d  \t RUNNABLE \t \n",p->name,p->pid);
+	 cprintf("%s \t %d  \t RUNNABLE \t %d \n",p->name,p->pid,p->sz);
 	}
   release(&ptable.lock);
   return 22;
